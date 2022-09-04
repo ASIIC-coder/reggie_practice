67b650d4b44614ae6add8b746c600f8df82a3b6e
aricleo <l3326135@163.com>
2022-09-04 10:56:07 +0800

    项目实战的框架代码第一次提交： 1.实体类、公共类的代码已添加 2.数据库表的表名本次提交为reggie

---
+++ src/main/java/com/ahg/reggie_project/ReggieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ahg.reggie_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReggieProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieProjectApplication.class, args);
+    }
+
+}
